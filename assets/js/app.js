818b46cbd42d75255cbeba86fcd5d583e7302eaf
tusharswami534 <tusharswami17@gmail.com>
2024-08-06 12:50:56 +0530

    add back to top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,16 @@ const navmenu = () => {
     document.getElementById("show-nav").classList.toggle("show-nav-bar")
 
 }
+let mybutton = document.getElementById("back-to-top");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
