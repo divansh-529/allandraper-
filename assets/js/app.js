31620c3d405265e493e104cdd92bb476bcca2b26
tusharswami534 <tusharswami17@gmail.com>
2024-08-07 11:24:23 +0530

    page complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,16 @@ const navmenu = () => {
 
 }
 let mybutton = document.getElementById("back-to-top");
+let colorchange = document.getElementById("header-section");
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         mybutton.style.display = "block";
+    } else if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        colorchange.style.background = "black"
     } else {
         mybutton.style.display = "none";
+        colorchange.style.background = "transparent"
     }
 }
 function topFunction() {
