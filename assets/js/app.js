c4754301ed27246e8ee7c5a5c47cbd6176c0533f
divansh-529 <divanshsachdev@gmail.com>
2024-08-07 12:36:42 +0530

    marding completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,3 +22,46 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+$(".slick-slider-parent").slick({
+  dots: true,
+  
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        dots: true,
+      },
+    },
+  ], 
+});
